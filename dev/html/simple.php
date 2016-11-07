d6741001935e50bc0219f43639022d9d277092e8
Amy Troppe <amy@medology.com>
2016-11-07 16:06:14 -0600

    testing loading gif with delay

--- dev/html/simple.php
+++ dev/html/simple.php
@@ -12,27 +12,44 @@
       max-width: 900px;
       padding: 10px;
     }
+    .loading-state {
+      height: 644px;
+      text-align: center;
+      align-items: center;
+      display: flex;
+      justify-content: space-around;
+    }
   </style>
 </head>
 <body>
 
   <h1>Find A Lab - Simple Mockups</h1>
-  <div id="simple-findalab">Loading Test Centers...</div>
+  <div id="simple-findalab">
+    <div class="loading-state">
+      <img class="loading-state-icon" src="/loading-gif.gif" alt="loading">
+    </div>
+
+  </div>
   <button class="findalab-reset" type="button">Reset findalab</button>
 
   <script src="/bower_components/jquery/dist/jquery.js"></script>
   <script src="https://maps.googleapis.com/maps/api/js?key=<?= env('GOOGLE_MAP_API_KEY'); ?>"></script>
   <script src="/js/findalab.js"></script>
   <script>
-  $('#simple-findalab').load('/template/findalab.html', function() {
-    var findalab = $(this).find('.findalab').findalab({
-      baseURL: 'http://findalab.local/fixtures/simple-mockups'
+
+  setTimeout(function(){
+
+    $('#simple-findalab').load('/template/findalab.html', function() {
+      var findalab = $(this).find('.findalab').findalab({
+        baseURL: 'http://findalab.local/fixtures/simple-mockups'
+      });
+
+      $('.findalab-reset').on('click', function() {
+        findalab.reset();
+      });
     });
 
-    $('.findalab-reset').on('click', function() {
-      findalab.reset();
-    });
-  });
+  }, 3000);
   </script>
 </body>
 </html>
