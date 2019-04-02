665ad771eb3388e36a93c48ad5497b51301c9420
Ramsey Tapia <ramsey@medology.com>
2019-04-02 18:13:29 -0500

    Setup yarn to handle components
    Updated node image
    updated deps

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,11 @@ var autoprefixerBrowsers = {
   browsers: ['last 2 versions', 'ie >= 9'],
 };
 
+gulp.task('js', function () {
+    return gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/js-cookie/src/js.cookie.js'])
+        .pipe(gulp.dest('js/lib/'));
+});
+
 gulp.task('sass', function() {
   return gulp.src('scss/findalab.scss')
   .pipe(sass(autoprefixerBrowsers))
